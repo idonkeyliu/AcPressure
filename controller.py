e0e0efcbddd8e4d93ad07e8f1229eefcac6a1980
idonkeyliu <idonkeyliu@gmail.com>
2017-03-16 23:29:27 +0800

    提交控制者

---
+++ controller.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+”“”
+控制者：打印压测完毕的信息，输出报告
+“”“
+
+class Controller():
+	def __init__(self):
+		pass
+	
+	def dump_info(info):
+		print '------------------------------'
+		print info
+		print '------------------------------'
